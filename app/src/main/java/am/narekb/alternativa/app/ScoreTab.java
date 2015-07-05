2ffcb8e8124d9f523258b25099be96a11bc06069
Narek Babajanyan <babaj96@gmail.com>
2015-07-05 14:04:27 +0400

    Added + buttons

--- app/src/main/java/am/narekb/alternativa/app/ScoreTab.java
+++ app/src/main/java/am/narekb/alternativa/app/ScoreTab.java
@@ -7,14 +7,18 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import am.narekb.alternativa.R;
 
-public class ScoreTab extends Fragment {
+public class ScoreTab extends Fragment implements View.OnClickListener {
 
     int ourPoints = 0, theirPoints = 0;
     TextView ourScore;
     TextView theirScore;
+
+    TextView addUs;
+    TextView addThem;
 
     public ScoreTab() {
         // Required empty public constructor
@@ -28,11 +32,24 @@ public class ScoreTab extends Fragment {
         View rootView = inflater.inflate(R.layout.score_tab, container, false);
         ourScore = (TextView) rootView.findViewById(R.id.our_score);
         theirScore = (TextView) rootView.findViewById(R.id.their_score);
+
+        addUs = (TextView) rootView.findViewById(R.id.add_us);
+        addUs.setOnClickListener(this);
+
+        addThem = (TextView) rootView.findViewById(R.id.add_them);
+        addThem.setOnClickListener(this);
+
         return rootView;
     }
 
     public void onClick(View v) {
-        //TODO: Set up both plus-sign onClicks to this method. Switch case through v.getId() and add scores accordingly
+        //Temporarily show Toast notification until NumberPicker dialog is implemented
+        if (v.getId() == R.id.add_us)
+            Toast.makeText(getActivity(), "Add points to us",Toast.LENGTH_SHORT).show();
+        else
+            Toast.makeText(getActivity(), "Add points to them",
+                    Toast.LENGTH_SHORT).show();
+
     }
 
 
